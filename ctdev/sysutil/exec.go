0d185b456a570522261b08a0046e4f6a1cf76d76
Thomas Conner <thomas.conner@connertechnology.io>
2026-03-15 09:12:44 -0400

    feat: add sysutil opts and exec helpers

---
+++ ctdev/sysutil/exec.go
@@ -0,0 +1,36 @@
+package sysutil
+
+import (
+	"fmt"
+	"os/exec"
+)
+
+// Run executes a command, routing output to opts.Stdout.
+// If opts.DryRun, prints the command without executing.
+func Run(o Opts, name string, args ...string) error {
+	if o.DryRun {
+		fmt.Fprintf(o.Stdout, "[dry-run] %s %s\n", name, joinArgs(args))
+		return nil
+	}
+	cmd := exec.Command(name, args...)
+	cmd.Stdout = o.Stdout
+	cmd.Stderr = o.Stdout
+	return cmd.Run()
+}
+
+// SudoRun executes a command with sudo.
+func SudoRun(o Opts, name string, args ...string) error {
+	sudoArgs := append([]string{name}, args...)
+	return Run(o, "sudo", sudoArgs...)
+}
+
+func joinArgs(args []string) string {
+	result := ""
+	for i, a := range args {
+		if i > 0 {
+			result += " "
+		}
+		result += a
+	}
+	return result
+}
