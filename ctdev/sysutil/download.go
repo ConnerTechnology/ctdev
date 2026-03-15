d137e4b7399e01a3af0bdc4f7842bc4934610728
Thomas Conner <thomas.conner@connertechnology.io>
2026-03-15 09:14:59 -0400

    feat: add HTTP download helper

---
+++ ctdev/sysutil/download.go
@@ -0,0 +1,30 @@
+package sysutil
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+// DownloadFile downloads a URL to a local file path.
+func DownloadFile(url, dest string) error {
+	resp, err := http.Get(url)
+	if err != nil {
+		return fmt.Errorf("download %s: %w", url, err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("download %s: HTTP %d", url, resp.StatusCode)
+	}
+
+	f, err := os.Create(dest)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	_, err = io.Copy(f, resp.Body)
+	return err
+}
